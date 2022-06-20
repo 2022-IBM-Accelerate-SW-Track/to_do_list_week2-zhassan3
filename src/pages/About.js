d2d9ed7e93cb12c3920af113d2a86d902b07934e
zhassan3 <zhassan3@mit.edu>
2022-06-19 22:55:55 -0400

    working on the project

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import imag from "../assets/zhheadshot.jpg";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
 export default class About extends Component {
   render() {
     return (
+
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
             <img
-              className="profile_image"
-              src={profile_pic}
+              class="profile_image"
+
               alt="Profile Pic"
-            ></img>
+              src={imag}
+
+            />
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Ziyad Hassan</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              <text>
+                Hello! My name is Ziyad Hassan and I am incoming sophmore studying computer science @ MIT. I run cross country and track for my school,but I also do it for fun
+                Asides from that I love taking walks along scenic places and taking pictures. I love getting to know people and making genuine connections, so feel free to hit me up!
+              </text>
             </div>
           </div>
         </div>
+
       </div>
-    );
+    )
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,13 +20,34 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
+
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+
+    const found = this.state.todos.find(element => element.content === todo.content);
+    console.log(found);
+    if (found !== undefined) {
+
+      return
+    }
+    else {
+
+
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +57,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
